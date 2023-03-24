618581b7c0ac48ee579a093c643ef9b86701ae82
Caleb Adeyemi <calebadeyemi@gmail.com>
2023-03-24 17:04:29 +0000

    Pushing latest commit in Prod to lower environments

--- script/adf_sync.sh
+++ script/adf_sync.sh
@@ -6,13 +6,13 @@ git config --global user.name "Caleb Adeyemi"
 
 eval `ssh-agent -s`
 
-git clone https://github.com/calebu/adf_qa.git
-cd adf_qa
 rm -rf adf_cicd/
 git clone https://github.com/calebu/adf_cicd.git
-rm -rf adf_cicd/.git*
 ls adf_cicd/
-cp -r adf_cicd/* .
+git clone https://github.com/calebu/adf_qa.git
+cd adf_qa
+rm -rf ../adf_cicd/.git*
+cp -r ../adf_cicd/* .
 #rm -rf adf_cicd/
 
 # Push to lower environment
